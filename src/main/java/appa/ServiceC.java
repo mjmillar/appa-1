237562cbfdc7d6eb0394c72832ececcb0e9ed356
bluemixtraining1 <bluemixtraining1@gmail.com>
2015-09-20 23:52:08 -0700

    Second fox to the Service C wsdl

--- src/main/java/appa/ServiceC.java
+++ src/main/java/appa/ServiceC.java
@@ -1,4 +1,11 @@
 package appa;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+@Stateless
+@LocalBean
+@WebService(serviceName = "ServiceCService", portName = "ServiceCPort", endpointInterface = "appa.IServiceC", targetNamespace = "http://appa/ServiceC")
 
 public class ServiceC implements IServiceC {
 
